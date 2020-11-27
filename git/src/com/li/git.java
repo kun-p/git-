ed86a6b6a8b85e12821bd1fd71c9f90afc960768
kun <484707125@qq.com>
2020-11-27 10:59:34 +0800

    把工程添加到git

---
+++ git/src/com/li/git.java
@@ -0,0 +1,12 @@
+package com.li;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
